9d404463e3bceed137fddd62699334bdb584bfd3
Muhammed Saeed <stringsaeed@gmail.com>
2023-05-31 01:47:39 +0300

    updates

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,7 +8,6 @@ export default function Layout() {
     <AuthProvider>
       <NotesProvider>
         <Stack
-          initialRouteName="(notes)"
           screenOptions={{
             headerTintColor: "#fff",
             headerTitleStyle: {
