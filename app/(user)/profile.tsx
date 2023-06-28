dbe72d6f8925bd6518fc5ab303cef54d6b4fed1c
Muhammed Saeed <stringsaeed@gmail.com>
2023-06-28 08:35:16 +0300

    feat(BRI-43): adding profile list UI

--- app/(user)/profile.tsx
+++ app/(user)/profile.tsx
@@ -1,27 +1,32 @@
 import React from "react";
+import { StyleSheet } from "react-native";
 
-import { Button, Input, ScreenContainer, Spacer, Spacing } from "components";
-import { useAuth } from "contexts";
+import { Button, ListItem, ScreenContainer, Spacer } from "components";
+// import { useAuth } from "contexts";
 import { useSignOutMutation } from "hooks";
 
 export default function Profile() {
-  const { user } = useAuth();
+  // const { user } = useAuth();
   const signOutMutation = useSignOutMutation();
 
   return (
     <ScreenContainer
-      handleSafeArea={"bottom"}
-      handleKeyboard
-      type="scroll"
-      centered
+      style={styles.container}
+      handleSafeArea="bottom"
+      // handleKeyboard
+      type="fixed"
     >
-      <Spacing />
-      <Input value={user?.email ?? ""} placeholder="Email" />
-      <Spacing />
-      <Input value={user?.phoneNumber ?? ""} placeholder="Phone Number" />
-      <Spacing />
-      <Input value={user?.displayName ?? ""} placeholder="Display Name" />
+      <ListItem isFirst size="lg" title="Account Information" />
+      <ListItem size="lg" title="Security Settings" />
+      <ListItem size="lg" title="Notifications Settings" />
+      <ListItem
+        isLast
+        size="lg"
+        title="Other Settings"
+        subtitle="theme, languages"
+      />
       <Spacer />
+
       <Button
         onPress={() => signOutMutation.mutate()}
         variant="warning"
@@ -30,3 +35,7 @@ export default function Profile() {
     </ScreenContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { paddingTop: 24 },
+});
