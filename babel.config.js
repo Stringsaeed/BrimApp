efa96ff6a1ed38dbb7957a89e388990a1b1444c0
Muhammed Saeed <stringsaeed@gmail.com>
2023-07-22 22:56:40 +0300

    short path and typed routes experiments

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,3 @@
-const rootImport = [
-  "module-resolver",
-  {
-    alias: {
-      components: "./components",
-      contexts: "./contexts",
-      stores: "./stores",
-      config: "./config",
-      utils: "./utils",
-      hooks: "./hooks",
-      types: "./types",
-      test: "./test",
-    },
-    extensions: [".ios.js", ".android.js", ".js", ".json", ".ts", ".tsx"],
-    root: ["."],
-  },
-];
-
 const router = require.resolve("expo-router/babel");
 
 const reanimated = "react-native-reanimated/plugin";
@@ -23,7 +5,7 @@ const reanimated = "react-native-reanimated/plugin";
 module.exports = function (api) {
   api.cache(true);
   return {
-    plugins: [rootImport, router, reanimated],
     presets: ["babel-preset-expo"],
+    plugins: [router, reanimated],
   };
 };
