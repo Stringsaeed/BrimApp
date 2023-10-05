c40f13c24cf5fdf2dc8d37b50adea71749fbc60d
Muhammed Saeed <stringsaeed@gmail.com>
2023-10-05 18:34:19 +0300

    fix firebase web secert

--- src/config/firebase/config.ts
+++ src/config/firebase/config.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   databaseURL:
     "https://brim-mobile-default-rtdb.europe-west1.firebasedatabase.app",
   appId: "1:374318773989:web:b43eb810131502bb75860c",
-  apiKey: "process.env.EXPO_PUBLIC_WEB_FIREBASE_API_KEY",
+  apiKey: process.env.EXPO_PUBLIC_WEB_FIREBASE_API_KEY,
   authDomain: "brim-mobile.firebaseapp.com",
   storageBucket: "brim-mobile.appspot.com",
   messagingSenderId: "374318773989",
