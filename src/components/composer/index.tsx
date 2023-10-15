041554c1aeb863f6d9dc10dcfc2a2ab980f64530
Muhammed Saeed <stringsaeed@gmail.com>
2023-10-15 08:08:13 +0400

    update composer with normal text area

--- src/components/composer/index.tsx
+++ src/components/composer/index.tsx
@@ -1,34 +1,17 @@
-import {
-  ChangeContentArgs,
-  RefRichTextEditor,
-  RichTextEditor,
-} from "@brim-app/composer";
 import React, { ForwardedRef, useImperativeHandle, useRef } from "react";
-import { useTheme } from "tamagui";
+import { TextInput } from "react-native";
+import { TextArea } from "tamagui";
 
-import styles from "./styles";
 import { ComposerComponentProps, ComposerRef } from "./types";
 import useNoteComposer from "./use-note-composer";
 
 function ComposerComponent(
-  { onLoadEnd }: ComposerComponentProps,
+  _: ComposerComponentProps,
   ref: ForwardedRef<ComposerRef>
 ) {
   const [focused, setFocused] = React.useState(false);
   const { onChangeText, value } = useNoteComposer();
-  const innerRef = useRef<RefRichTextEditor>(null);
-  const theme = useTheme();
-
-  const htmlStyles = {
-    CSS: `
-      html {
-        color: ${theme.color.get()};
-        font-size: 16px;
-      }
-      `,
-    placeholderColor: theme.gray10.get(),
-    caretColor: theme.color.get(),
-  };
+  const innerRef = useRef<TextInput>(null);
 
   const onFocus = () => {
     setFocused(true);
@@ -36,10 +19,6 @@ function ComposerComponent(
 
   const onBlur = () => {
     setFocused(false);
-  };
-
-  const onChangeContent = ({ html }: ChangeContentArgs) => {
-    onChangeText(html);
   };
 
   useImperativeHandle(ref, () => {
@@ -57,18 +36,18 @@ function ComposerComponent(
   });
 
   return (
-    <RichTextEditor
-      containerStyle={styles.containerStyle}
-      onChangeContent={onChangeContent}
+    <TextArea
+      onChangeText={onChangeText}
       placeholder="Write something..."
-      initialHTMLContent={value}
-      htmlStyles={htmlStyles}
-      onLoadEnd={onLoadEnd}
-      style={styles.style}
+      value={value}
       onFocus={onFocus}
       onBlur={onBlur}
       ref={innerRef}
       scrollEnabled
+      bg="$backgroundTransparent"
+      borderRadius="$0"
+      flex={1}
+      borderWidth="$0"
     />
   );
 }
