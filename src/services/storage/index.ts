562f5b0be2c6440a7ee88795d6f495a6ca39883e
Muhammed Saeed <stringsaeed@gmail.com>
2024-02-04 18:48:20 +0200

    Refactor AsyncStorage methods to return promises

--- src/services/storage/index.ts
+++ src/services/storage/index.ts
@@ -3,7 +3,8 @@ import { MMKV } from "react-native-mmkv";
 export const storage = new MMKV();
 
 export const AsyncStorage = {
-  setItem: (key: string, value: string) => storage.set(key, value),
-  getItem: (key: string) => storage.getString(key) ?? null,
-  removeItem: (key: string) => storage.delete(key),
+  setItem: (key: string, value: string) =>
+    Promise.resolve(storage.set(key, value)),
+  getItem: (key: string) => Promise.resolve(storage.getString(key) ?? null),
+  removeItem: (key: string) => Promise.resolve(storage.delete(key)),
 };
